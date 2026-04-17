5bfbf155d2eb301b4bc726c4607a4d1e55d51606
haamzzaa <hamzamr48@gmail.com>
2026-04-17 21:27:26 +0530

    changing views of accounts to remove 500 error

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth import authenticate
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
@@ -23,6 +25,26 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'email', 'username']
 
+
 class CustomTokenSerializer(TokenObtainPairSerializer):
     username_field = 'email'
 
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,  # Django still uses username internally
+            password=password
+        )
+
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+
+        data = super().validate({
+            "username": user.username,
+            "password": password
+        })
+
+        return data
